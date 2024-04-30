ea73d94ec25172a5fa6e5626b07e9987e1077756
amosISA <amosisa700@gmail.com>
2024-04-30 08:25:31 +0200

    feat: start animations course on angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,52 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterOutlet],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  animations: [
+    trigger('myAnimation', [
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'red'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.8,
+        backgroundColor: 'yellow'
+      })),
+      transition('open => closed', [animate('1s')]),
+      transition('closed => open', [animate('0.1s')]),
+    ]),
+    trigger('enterLeave', [
+      transition(':enter', [
+        style({
+          opacity: 0,
+          transform: 'translateY(10px)'
+        }),
+        animate('500ms', style({
+          opacity: 1,
+          transform: 'translateY(0px)'
+        }))
+      ]),
+      transition(':leave', [
+        animate('500ms', style({
+          opacity: 0,
+          transform: 'translateY(10px)'
+        }))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'angular-animations';
+  enterLeave = signal(false);
+  isOpen = signal(true);
+  toggle(): void {
+    this.isOpen.set(!this.isOpen());
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(appRoutes),
+    provideAnimations()
+  ],
 };
+
+// old value .... new value
+// before v4
+// eager loaded
