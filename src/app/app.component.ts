e170df1e6ac43a016d0771b35b63cb05676ed00a
amosISA <amosisa700@gmail.com>
2024-05-03 06:50:56 +0200

    feat: advanced-animations query and animatechild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,66 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component, signal } from '@angular/core';
+import {
+  animate,
+  animateChild,
+  query,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+import { Component, HostBinding, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+// query() + animate()
+const shakeAnim = [
+  style({ transform: 'rotate(0)' }),
+  animate('0.1s', style({ transform: 'rotate(2deg)' })),
+  animate('0.1s', style({ transform: 'rotate(-2deg)' })),
+  animate('0.1s', style({ transform: 'rotate(2deg)' })),
+  animate('0.1s', style({ transform: 'rotate(0)' })),
+];
+
+export const EnterExitLeft = [
+  trigger('enterExitLeft', [
+    transition(':enter', [
+      style({ opacity: 0, transform: 'translateX(-200px)' }),
+      animate(
+        '300ms ease-in',
+        style({ opacity: 1, transform: 'translateX(0)' })
+      ),
+    ]),
+    transition(':leave', [
+      animate(
+        '300ms ease-in',
+        style({ opacity: 0, transform: 'translateX(-200px)' })
+      ),
+    ]),
+  ]),
+];
+
+export const EnterExitRight = [
+  trigger('enterExitRight', [
+    transition(':enter', [
+      style({ opacity: 0, transform: 'translateX(200px)' }),
+      animate(
+        '300ms ease-in',
+        style({ opacity: 1, transform: 'translateX(0)' })
+      ),
+    ]),
+    transition(':leave', [
+      animate(
+        '300ms ease-in',
+        style({ opacity: 0, transform: 'translateX(200px)' })
+      ),
+    ]),
+  ]),
+];
+
+export const Container = [
+	trigger('container', [
+		transition(':enter, :leave', [
+			query('@*', animateChild()),
+		]),
+	]),
+];
 
 @Component({
   standalone: true,
@@ -9,44 +69,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   animations: [
-    trigger('myAnimation', [
-      state('open', style({
-        height: '200px',
-        opacity: 1,
-        backgroundColor: 'red'
-      })),
-      state('closed', style({
-        height: '100px',
-        opacity: 0.8,
-        backgroundColor: 'yellow'
-      })),
-      transition('open => closed', [animate('1s')]),
-      transition('closed => open', [animate('0.1s')]),
-    ]),
-    trigger('enterLeave', [
-      transition(':enter', [
-        style({
-          opacity: 0,
-          transform: 'translateY(10px)'
-        }),
-        animate('500ms', style({
-          opacity: 1,
-          transform: 'translateY(0px)'
-        }))
+    // With query
+    trigger('queryAnimation', [
+      transition('* => sinLimite', [
+        query('.card', shakeAnim, { optional: true }),
       ]),
-      transition(':leave', [
-        animate('500ms', style({
-          opacity: 0,
-          transform: 'translateY(10px)'
-        }))
-      ])
-    ])
-  ]
+      transition('* => conLimite', [
+        query('.card', shakeAnim, { limit: 2, optional: true }),
+      ]),
+    ]),
+
+    // With query + childs
+    Container, EnterExitLeft, EnterExitRight
+  ],
 })
 export class AppComponent {
-  enterLeave = signal(false);
-  isOpen = signal(true);
-  toggle(): void {
-    this.isOpen.set(!this.isOpen());
-  }
+  animation = signal<string | null>(null); // sinLimite / conLimite
+  isDisplayed = signal(true);
+
+  @HostBinding('@.disabled') public animationsDisabled = true;
 }
