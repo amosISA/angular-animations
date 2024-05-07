71b89f5239724a72be960a9fb5ac37e0b320f8a1
amosISA <amosisa700@gmail.com>
2024-05-07 05:41:18 +0200

    feat: route transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,21 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { UpperCasePipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { ChildrenOutletContexts, Data, RouterLink, RouterOutlet } from '@angular/router';
+import { slideInAnimation } from './slideInAnimation';
 
 @Component({
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UpperCasePipe, RouterLink],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  animations: [
-    trigger('myAnimation', [
-      state('open', style({
-        height: '200px',
-        opacity: 1,
-        backgroundColor: 'red'
-      })),
-      state('closed', style({
-        height: '100px',
-        opacity: 0.8,
-        backgroundColor: 'yellow'
-      })),
-      transition('open => closed', [animate('1s')]),
-      transition('closed => open', [animate('0.1s')]),
-    ]),
-    trigger('enterLeave', [
-      transition(':enter', [
-        style({
-          opacity: 0,
-          transform: 'translateY(10px)'
-        }),
-        animate('500ms', style({
-          opacity: 1,
-          transform: 'translateY(0px)'
-        }))
-      ]),
-      transition(':leave', [
-        animate('500ms', style({
-          opacity: 0,
-          transform: 'translateY(10px)'
-        }))
-      ])
-    ])
-  ]
+  animations: [slideInAnimation]
 })
 export class AppComponent {
-  enterLeave = signal(false);
-  isOpen = signal(true);
-  toggle(): void {
-    this.isOpen.set(!this.isOpen());
+  routes = signal(['home', 'about', 'profile']);
+  contexts = inject(ChildrenOutletContexts);
+
+  getRouteAnimationData(): Data | undefined {
+    return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Route } from '@angular/router';
+import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about.component';
+import { HomeComponent } from './components/home.component';
+import { ProfileComponent } from './components/profile.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, data: { animation: 'home' } },
+  { path: 'about', component: AboutComponent, data: { animation: 'about' } },
+  { path: 'profile', component: ProfileComponent, data: { animation: 'profile' } },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
